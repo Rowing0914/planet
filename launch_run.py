5f08b33d42df3cb4193709174c0ff4fabd1c830f
Rowing0914 <kosakaboat@gmail.com>
2023-04-22 21:46:27 +0900

    minor updates

--- launch_run.py
+++ launch_run.py
@@ -14,16 +14,16 @@ for row in df_params.iterrows():
     dict_param = row[1].to_dict()
     list_params.append(' '.join([f'--{k}={v}' for k, v in dict_param.items() if k != "name"]))
 
-if df_params["env"][0].startswith("mujoco"):
-    print("================ START: prep mujoco ====================")
-    subprocess.Popen("sh ./prep_mujoco.sh", shell=True).wait()
-    import os
+# if df_params["env"][0].startswith("mujoco"):
+print("================ START: prep mujoco ====================")
+subprocess.Popen("sh ./prep_mujoco.sh", shell=True).wait()
+import os
 
-    os.environ["LD_LIBRARY_PATH"] = f"{os.environ.get('LD_LIBRARY_PATH')}:/home/nsml/.mujoco/mujoco210/bin"
-    # import mujoco_py  # this is to fully compile the mujoco for the following main exp
-    # this is to fully compile the mujoco for the following main exp
-    subprocess.Popen("python ./_mujoco_demo.py", shell=True).wait()
-    print("================ END: prep mujoco ====================")
+os.environ["LD_LIBRARY_PATH"] = f"{os.environ.get('LD_LIBRARY_PATH')}:/home/nsml/.mujoco/mujoco210/bin"
+# import mujoco_py  # this is to fully compile the mujoco for the following main exp
+# this is to fully compile the mujoco for the following main exp
+subprocess.Popen("python ./_mujoco_demo.py", shell=True).wait()
+print("================ END: prep mujoco ====================")
 
 processes = list()
 for i, params in enumerate(list_params):
